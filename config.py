213835e06c730278a79539bd969f86bb7b63da00
Nelson Rodrigues <nelsonrds95@gmail.com>
2020-04-18 23:04:01 +0100

    Removed Cached files

--- config.py
+++
@@ -1,5 +0,0 @@
-SECRET_KEY = "DEV"
-DEBUG = True # Turns on debugging features in Flask
-DB_MONGOOSE = "mongodb+srv://nelsonrds:connect98@cluster0-lq52p.mongodb.net/mongo-dev-db?retryWrites=true&w=majority"
-
-print("run config")
